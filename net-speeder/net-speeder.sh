3a89aae0da15c58c9642c08022337caf1b34c2e6
labulaka521 <labulaka521@live.cn>
2017-03-15 15:06:16 -0400

    1.3

--- net-speeder/net-speeder.sh
+++ net-speeder/net-speeder.sh
@@ -4,7 +4,6 @@
 # mail:labulaka521@live.cn
 # Created Time: 2017-03-15
 
-apt-get update
 apt-get install libpcap0.8 libnet1-dev  libpcap0.8-dev libnet1 gcc 
 wget https://github.com/snooda/net-speeder/archive/master.zip
 unzip master.zip
