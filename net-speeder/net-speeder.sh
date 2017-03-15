451b99b0bdf3fd0f97acdc9b8ccfb5d3fc82eff4
labulaka521 <labulaka521@live.cn>
2017-03-15 15:09:04 -0400

    1.4

--- net-speeder/net-speeder.sh
+++ net-speeder/net-speeder.sh
@@ -4,10 +4,14 @@
 # mail:labulaka521@live.cn
 # Created Time: 2017-03-15
 
+
 apt-get install libpcap0.8 libnet1-dev  libpcap0.8-dev libnet1 gcc -y
 wget https://github.com/snooda/net-speeder/archive/master.zip
 unzip master.zip
 cd net-speeder-master/
 sh build.sh -DCOOKED
 nohup ./net_speeder eth0 "ip" &
-
+cd
+wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-go.sh
+chmod +x shadowsocks-go.sh
+./shadowsocks-go.sh 2>&1 | tee shadowsocks-go.log
